bca53cba5f18ce8ecacc8198499f187520b4eec4
tuancq <tuancq2011@gmail.com>
2022-05-24 11:52:27 +0700

    create scriptable object and displayitem script

--- Assets/Scripts/ItemDisplay.cs
+++ Assets/Scripts/ItemDisplay.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemDisplay : MonoBehaviour
 {
+    public Item item;
+    public Image image;
+    public Text quantity;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        image.sprite = item.Icon;
+        quantity.text = item.Quantity.ToString();
     }
 }
